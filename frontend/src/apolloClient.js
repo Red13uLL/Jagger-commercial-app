91d48cdf7f3fd32c9909cd2bd26ce203c2d4f502
Marko Obrenovic <igor.kostic@bad-team.net>
2026-01-15 14:41:01 +0100

    Add Netlify config and normalize GraphQL URL

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,6 +1,20 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
-const graphqlUrl = process.env.GRAPHQL_URL || 'http://127.0.0.1:3000/graphql';
+const defaultGraphqlUrl = 'http://127.0.0.1:3000/graphql';
+const rawGraphqlUrl = process.env.GRAPHQL_URL || defaultGraphqlUrl;
+
+const normalizeGraphqlUrl = (value) => {
+  if (!value) {
+    return defaultGraphqlUrl;
+  }
+
+  const withProtocol = /^[a-z]+:\/\//i.test(value) ? value : `https://${value}`;
+  const normalizedBase = withProtocol.replace(/\/$/, '');
+
+  return normalizedBase.includes('/graphql') ? normalizedBase : `${normalizedBase}/graphql`;
+};
+
+const graphqlUrl = normalizeGraphqlUrl(rawGraphqlUrl);
 
 const cache = new InMemoryCache({
   typePolicies: {
